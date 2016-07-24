dd454ef3b92bee4883a4aebb43f9435c230dc5fa
RYUJI WATANABE <oga@5K-iMac-Late2014.local>
2016-07-24 16:18:02 +0900

    update build.sh

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ agvtool new-version -all ${BUILD_NUMBER}
 
 # アーカイブ
 xcodebuild clean archive -project ${PROJECT_NAME}.xcodeproj -target $TARGET -archivePath Archive
+xcodebuild -exportArchive -exportFormat ipa -archivePath ./Archive.xcarchive -exportPath Sample.ipa -exportProvisioningProfile 'XC: com.example'
 
 # IPA
-xcodebuild -exportArchive -exportFormat IPA -archivePath ${WORKSPACE}/Archive.xcarchive -exportPath MorningInHouse-${VERSION}.ipa -exportProvisioningProfile ${PROVISIONING}
+xcodebuild -exportArchive -exportFormat IPA -archivePath ${WORKSPACE}/Archive.xcarchive -exportPath ${PROJECT_NAME}-${VERSION}.ipa -exportProvisioningProfile ${PROVISIONING}
