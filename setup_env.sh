8ec63d0a2f3a6da519dc18f92bc05de0e538587c
RYUJI WATANABE <oga@5K-iMac-Late2014.local>
2016-09-03 15:43:18 +0900

    update project

--- setup_env.sh
+++ setup_env.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
 # PROJECT
-PROJECT_NAME='AsciiArt'
-BUILD_MODE='Release'
-PROVISIONING='XC:AdHocAsciiArt'
-FL_CARTHAGE_PLATFORM="iOS"
+export PROJECT_NAME='AsciiArt'
+export BUILD_MODE='Release'
+export PROVISIONING='XC:AdHocAsciiArt'
 
 # Fixed Env
-XCODE_VER=""
+export XCODE_VER=""
 if [ -e @{${WORKSPACE}/XCVERSION ]; then
-XCODE_VER="$(cat ${WORKSPACE}/XCVERSION)"
+export XCODE_VER="$(cat ${WORKSPACE}/XCVERSION)"
 fi
-TARGET_XCODE=/Applications/Xcode${XCODE_VER}.app
-DEVELOPER_DIR=${TARGET_XCODE}/Contents/Developer/
-VERSION=` agvtool mvers -terse | grep -i ${PROJECT_NAME}/Info.plist | grep -o -e '[0-9]\+\.[0-9]\+\.[0-9]\+'`
-IPA_FILE=${WORKSPACE}/${PROJECT_NAME}-${VERSION}-${BUILD_NUMBER}.ipa
-EXPORT_OPTIONS_PLIST="export_options.plist"
+export TARGET_XCODE=/Applications/Xcode${XCODE_VER}.app
+export DEVELOPER_DIR=${TARGET_XCODE}/Contents/Developer/
+export VERSION=` agvtool mvers -terse | grep -i ${PROJECT_NAME}/Info.plist | grep -o -e '[0-9]\+\.[0-9]\+\.[0-9]\+'`
+export IPA_FILE=${WORKSPACE}/${PROJECT_NAME}-${VERSION}-${BUILD_NUMBER}.ipa
+export EXPORT_OPTIONS_PLIST="export_options.plist"
